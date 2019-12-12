52aeeaba704815f30789163e5fd43e9ff1bbbd57
suzanne werner <capnsue@gmail.com>
2019-12-12 15:18:23 -0500

    made blocks move

--- py/breaky/break1.py
+++ py/breaky/break1.py
@@ -1,4 +1,7 @@
 # starting template to make breakout clone
+# with thanks to programarcadegames.com
+
+
 
 
 import pygame
@@ -45,21 +48,35 @@ class Block(pygame.sprite.Sprite):
 
         # get rectangle of image that has the dimensions
         self.rect = self.image.get_rect()
+    
+    def update(self):
+        # called each frame
+        # move block down one pixel
+        self.rect.y += 1
+        # if blocks go off the bottom of the screen,
+        # send them back to the top
+        if self.rect.y > screen_height + 100: # docs say 410, why
+            self.reset_pos()
+ 
+
+    def reset_pos(self):
+        # reset position at top of screen at random x location
+        # called by update() or if there is a collision
+        self.rect.y = random.randrange(-300, -20) # i dont understand why these numbers
+        self.rect.x = random.randrange(0, screen_width)
+
+
+
+
 ############################################################################################################
-
-
-
 
 pygame.init()
 
-
-
-
-WIDTH = 800
-HEIGHT = 600
+screen_width = 800
+screen_height = 600
 
 # Set the width and height of the screen [width, height]
-size = (WIDTH, HEIGHT)
+size = (screen_width, screen_height)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("breaky 1")
@@ -73,8 +90,8 @@ for i in range(50):
     block = Block(BLACK, 20, 15)
 
     # put in random location
-    block.rect.x = random.randrange(WIDTH)
-    block.rect.y = random.randrange(HEIGHT)
+    block.rect.x = random.randrange(screen_width)
+    block.rect.y = random.randrange(screen_height)
 
     # add to list of sprites
     block_list.add(block)
@@ -122,12 +139,21 @@ while running:
 
     # check if player block collided with any of the block in block_list
     # if so, kill the block
-    blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)
+    #'''spritecollide(sprite, group, dokill, collided = None) -> Sprite_list
+    blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
     
     # give them a score for each block they hit
     for block in blocks_hit_list:
         score += 1
         print(score)
+
+        #reset block to top of screen
+        block.reset_pos()
+        
+
+    # update position of all blocks in block_list
+    block_list.update()
+
         
 # --------------------------------------------------
     # draw stuff on screen!
@@ -167,37 +193,3 @@ pygame.quit()
 
 
  
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
- 
-    # --- Game logic should go here
- 
-    # --- Screen-clearing code goes here
- 
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
- 
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-    screen.fill(WHITE)
- 
-    # --- Drawing code should go here
- 
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
- 
-# Close the window and quit.
-pygame.quit()
